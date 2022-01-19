797c81b03e462529adfef1640abd7371d5e8abd2
DESKTOP-F2Q5FN3\guillaume <riguigui@hotmail.com>
2022-01-19 11:07:38 +0100

    welcome modal
    welcome modal is ok

--- src/components/welcome/WelcomeModals.js
+++ src/components/welcome/WelcomeModals.js
@@ -5,18 +5,33 @@ import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../context/UserContext";
 import { db, storage } from "../../firebase/firebaseConfig";
+import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowBack from "@mui/icons-material/ArrowBack";
 
+const customStyleArrow = {
+  fontSize: "75px",
+  color: "#e63946",
+  cursor: "pointer",
+};
 
-
-const WelcomeModals = ({setDiplayWelcomeModals}) => {
+const WelcomeModals = ({ setDiplayWelcomeModals }) => {
   const [file, setFile] = useState("");
   const [name, setName] = useState("");
+  const [volume, setVolume] = useState("");
   const [selectedRadio, setSelectedRadio] = useState("");
-  const radios = ["recifal","mixte","fish-only"];
-  const volume = useRef();
+  const radios = ["recifal", "mixte", "fish-only"];
 
   const { currentUser } = useContext(UserContext);
-const navigate=useNavigate()
+  const navigate = useNavigate();
+
+  // display forms step by step
+  const [displayName, setDisplayName] = useState(true);
+  const [displayVolume, setDisplayVolume] = useState(false);
+  const [displayType, setDisplayType] = useState(false);
+  const [displayPhoto, setDisplayPhoto] = useState(false);
+  const [displayValidation, setDisplayValidation] = useState(false);
+
   const onFileChange = (e) => {
     e.preventDefault();
     setFile(e.target.files[0]);
@@ -31,63 +46,147 @@ const navigate=useNavigate()
     // permet de stocker url dans firestore
     await getDownloadURL(storageRef).then((url) => {
       addDoc(collection(db, "users", currentUser.uid, "infos-aqua"), {
-        url,
+        url: url,
         time: serverTimestamp(),
-        name,
-        volume: volume.current.value,
+        name: name,
+        volume: volume,
+        selectedRadio: selectedRadio,
       });
     });
     e.target.reset();
+    navigate("/private/dashboard");
   };
 
   const closeModals = () => {
- setDiplayWelcomeModals(false)
+    setDiplayWelcomeModals(false);
   };
 
   return (
-    <div  className="fixed-container-modals">
+    <div className="fixed-container-modals">
       <div onClick={closeModals} className="container-modals"></div>
 
-      <form
-        onSubmit={handleSubmitnewFish}
-        className="welcome-form"
-      >
-          <div> Bienvenue sur Aqua Reef, si vous avez déjà les informations sur votre aquarium, vous pouvez
-    remplir ce petit formulaire sinon clickez sur passer cette étape
-     </div>
-      <button onClick={()=>navigate('/private/dashboard')}>Passer cette étape </button>
-        <label htmlFor="name">Donnez un nom à votre Aquarium</label>
-        <input
-          onChange={(e) => setName(e.target.value)}
-          type="text"
-          id="name"
-          placeholder=""
-        />
+      <form onSubmit={handleSubmitnewFish} className="welcome-form">
+        {displayName && (
+          <>
+            <div>
+              {" "}
+              <h2>
+                Bienvenue sur Aqua Reef <br></br>
+              </h2>
+            </div>
 
-        <label for="volume">Quel est le voulume de votre Aquarium ? </label>
-        <input ref={volume} type="text" id="volume" placeholder="" />
-
-        <label for="type">Quel est le type ? </label>
-        {radios.map((radio) => {
-          return (
-            <li key={radio}>
-            <label htmlFor={radio}>{radio}</label>
+            <label htmlFor="name">Donnez un nom à votre Aquarium</label>
             <input
-              type="radio"
-              value={radio}
-              id={radio}
-              checked={radio === selectedRadio}
-              onChange={(e) => setSelectedRadio(e.target.value)}
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              id="name"
+              placeholder=""
             />
-            
-          </li>
-          );
-        })}
+            <div className="container-arrow first-arrow">
+              <ArrowRightAltIcon
+                sx={customStyleArrow}
+                onClick={() => {
+                  setDisplayVolume(true);
+                  setDisplayName(false);
+                }}
+              />
+            </div>
+          </>
+        )}
 
-        <label htmlFor="photo">Ajoutez une photo</label>
-        <input type="file" onChange={onFileChange} />
+        {displayVolume && (
+          <>
+            <label for="volume">Quel est le volume de votre Aquarium ? </label>
+            <input
+              onChange={(e) => setVolume(e.target.value)}
+              type="text"
+              id="volume"
+              placeholder=""
+            />
+            <div className="container-arrow">
+              <span
+                onClick={() => {
+                  setDisplayName(true);
+                  setDisplayVolume(false);
+                }}
+              >
+                Revenir en arrière
+              </span>
+              <ArrowRightAltIcon
+                sx={customStyleArrow}
+                onClick={() => {
+                  setDisplayType(true);
+                  setDisplayVolume(false);
+                }}
+              />
+            </div>
+          </>
+        )}
+        {displayType && (
+          <>
+            <label for="type">Quel est le type ? </label>
+            {radios.map((radio) => {
+              return (
+                <li key={radio}>
+                  <label htmlFor={radio}>{radio}</label>
+                  <input
+                    type="radio"
+                    value={radio}
+                    id={radio}
+                    checked={radio === selectedRadio}
+                    onChange={(e) => setSelectedRadio(e.target.value)}
+                  />
+                </li>
+              );
+            })}
+            <div className="container-arrow">
+              <span
+                onClick={() => {
+                  setDisplayType(false);
+                  setDisplayVolume(true);
+                }}
+              >
+                Revenir en arrière
+              </span>
+              <ArrowRightAltIcon
+                sx={customStyleArrow}
+                onClick={() => {
+                  setDisplayType(false);
+                  setDisplayPhoto(true);
+                }}
+              />
+            </div>
+          </>
+        )}
 
-        <button type="submit">Enregistrer</button>
+        {displayPhoto && (
+          <>
+            <label htmlFor="photo">Ajoutez une photo</label>
+            <input type="file" onChange={onFileChange} />
+            <div className="container-arrow">
+              <span
+                onClick={() => {
+                  setDisplayType(true);
+                  setDisplayPhoto(false);
+                }}
+              >
+                Revenir en arrière
+              </span>
+              <ArrowRightAltIcon
+                sx={customStyleArrow}
+                onClick={() => {
+                  setDisplayPhoto(false);
+                  setDisplayValidation(true);
+                }}
+              />
+            </div>
+          </>
+        )}
+        {displayValidation && (
+          <>
+            <button type="submit">Merci et Bonne découverte </button>
+          </>
+        )}
       </form>
     </div>
   );
